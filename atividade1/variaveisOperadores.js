0ac88835b45a56f79b926130414d63d1c0bf0feb
LUCAS DOS SANTOS FÉLIX <lucas.santos@a.ucb.br>
2023-08-10 10:29:13 -0300

    adiciona atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+var n1 = 5
+var n2 = 2
+
+var n3 = n1 + n2
+
+console.log (n3)
+
+var n3 = n1 - n2
+
+console.log (n3)
+
+var n3 = n1 * n2
+
+console.log (n3)
+
+var n3 = n1 / n2
+
+console.log (n3)
+
