cbac344eed97d8b0ced1d4e27916eb1e56397d1f
LUCAS DOS SANTOS FÉLIX <lucas.santos@a.ucb.br>
2023-08-10 10:43:49 -0300

    adiciona atividade 1

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -3,17 +3,16 @@ var n2 = 2
 
 var n3 = n1 + n2
 
-console.log (n3)
+console.log ('Resultado da soma entre ' +n1, 'e ' +n2, '= ' +n3)
 
 var n3 = n1 - n2
 
-console.log (n3)
+console.log ('Resultado da subtracao entre ' +n1, 'e ' +n2, '= ' +n3)
 
 var n3 = n1 * n2
 
-console.log (n3)
+console.log ('Resultado da multiplicacao entre ' +n1, 'e ' +n2, '= ' +n3)
 
 var n3 = n1 / n2
 
-console.log (n3)
-
+console.log ('Resultado da divisao entre ' +n1, 'e ' +n2, '= ' +n3)
